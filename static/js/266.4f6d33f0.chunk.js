fff6f958032bed46ea0fae91631b2fba98753def
Lukast2008 <111371912+Lukast2008@users.noreply.github.com>
2023-01-03 16:29:03 +0000

    Deploying to gh-pages from @ Lukast2008/goit-react-hw-08-phonebook@23acf0a4699fac3a3955ae1a9a00c0efc7ed77f3 🚀

---
+++ static/js/266.4f6d33f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"PhoneBook"})})}}}]);
+//# sourceMappingURL=266.4f6d33f0.chunk.js.map
